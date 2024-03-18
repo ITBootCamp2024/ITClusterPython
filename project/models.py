3096862cb48590e8231ffe8cf325d0c4952f7aae
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-03-19 01:13:25 +0200

    Revert "added connection to database and routes on programs_levels"

--- project/models.py
+++
@@ -1,6 +0,0 @@
-from project.extensions import db
-
-
-class ProgramLevel(db.Model):
-    id: int = db.Column(db.Integer, primary_key=True)
-    name: str = db.Column(db.String(255), nullable=False)
