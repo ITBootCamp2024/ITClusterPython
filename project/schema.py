9d1e13e0fbca667500f61e54148173e796318106
Maksym Bezhevets <begik4@gmail.com>
2024-03-19 16:12:43 +0200

    Deleted city (test namespace)

--- project/schema.py
+++ project/schema.py
@@ -22,13 +22,12 @@ course_blocks_model = api.model(
     },
 )
 
-# program_level_input_model = api.model("ProgramLevelInput", {"name": fields.String})
-
-
 specialty_model = api.model(
     "Specialty",
     {
-        "id": fields.Integer(readonly=True, description="The specialty unique identifier"),
+        "id": fields.Integer(
+            readonly=True, description="The specialty unique identifier"
+        ),
         "name": fields.String(required=True, description="The name of the specialty"),
         "link_standart": fields.String(
             required=True, description="The link to the specialty"
