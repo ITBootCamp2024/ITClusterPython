e6e3df0aa17d6674d85a834f69392a0e1e364164
Maksym Bezhevets <begik4@gmail.com>
2024-03-19 11:53:37 +0200

    Fix bug with utf-8

--- project/__init__.py
+++ project/__init__.py
@@ -10,6 +10,10 @@ from project.models import ProgramLevel, CourseBlocks
 def create_app():
     app = Flask(__name__)
 
+    # Flask to use UTF-8 encoding
+    app.config["JSON_AS_ASCII"] = False
+    app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
     api.init_app(app)
