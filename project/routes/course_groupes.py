97c8a75e8d6d62284ae167c637034fa691633f96
Maksym Bezhevets <begik4@gmail.com>
2024-03-22 23:05:24 +0200

    Updated course_groupes

--- project/routes/course_groupes.py
+++ project/routes/course_groupes.py
@@ -1,23 +1,31 @@
 from flask_restx import Resource, Namespace, abort
 
-from project.extensions import db
-from project.schema import course_groupes_model
+from project.extensions import db, pagination
+from project.schema import (
+    course_groupes_model,
+    pagination_parser,
+    custom_schema_pagination,
+)
 from project.models import CourseGroupes
 
 course_groupes_ns = Namespace(name="course_groupes", description="Course_groupes")
 
 
 @course_groupes_ns.route("/")
+@course_groupes_ns.response(200, model=[course_groupes_model], description="Success")
 class CourseGroupesList(Resource):
     """Shows a list of all course_groupes, and lets you POST to add new program level"""
 
-    @course_groupes_ns.marshal_list_with(course_groupes_model)
+    @course_groupes_ns.expect(pagination_parser)
     def get(self):
         """List all course_groupes levels"""
-        return CourseGroupes.query.all()
+        return pagination.paginate(
+            CourseGroupes,
+            course_groupes_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
 
-    @course_groupes_ns.expect(course_groupes_model)
-    @course_groupes_ns.marshal_list_with(course_groupes_model)
+    @course_groupes_ns.expect(course_groupes_model, pagination_parser)
     def post(self):
         """Create a new course_groupes"""
         course_gp = CourseGroupes(
@@ -27,7 +35,11 @@ class CourseGroupesList(Resource):
         )
         db.session.add(course_gp)
         db.session.commit()
-        return CourseGroupes.query.all(), 201
+        return pagination.paginate(
+            CourseGroupes,
+            course_groupes_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
 
 
 def get_course_groupes_or_404(id):
@@ -38,6 +50,7 @@ def get_course_groupes_or_404(id):
 
 
 @course_groupes_ns.route("/<int:id>/")
+@course_groupes_ns.response(200, model=[course_groupes_model], description="Success")
 @course_groupes_ns.response(404, "The course group not found")
 @course_groupes_ns.param("id", "The course group unique identifier")
 class ProgramLevelDetail(Resource):
@@ -48,8 +61,7 @@ class ProgramLevelDetail(Resource):
         """Fetch a given course group"""
         return get_course_groupes_or_404(id)
 
-    @course_groupes_ns.expect(course_groupes_model)
-    @course_groupes_ns.marshal_list_with(course_groupes_model)
+    @course_groupes_ns.expect(course_groupes_model, pagination_parser)
     def put(self, id):
         """Update a course group given its identifier"""
         course_gp = get_course_groupes_or_404(id)
@@ -57,17 +69,20 @@ class ProgramLevelDetail(Resource):
         course_gp.description = course_groupes_ns.payload["description"]
         course_gp.type_id = course_groupes_ns.payload["type_id"]
         db.session.commit()
-        return CourseGroupes.query.all()
+        return pagination.paginate(
+            CourseGroupes,
+            course_groupes_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
 
-    @course_groupes_ns.response(
-        200,
-        model=[course_groupes_model],
-        description="Object successfully deleted, returns a list without an object",
-    )
-    @course_groupes_ns.marshal_list_with(course_groupes_model)
+    @course_groupes_ns.expect(pagination_parser)
     def delete(self, id):
         """Delete a course group given its identifier"""
         course_gp = get_course_groupes_or_404(id)
         db.session.delete(course_gp)
         db.session.commit()
-        return CourseGroupes.query.all(), 200
+        return pagination.paginate(
+            CourseGroupes,
+            course_groupes_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
