0fe558e6211f71cc46daa366da78d6acf0d1bba5
Maksym Bezhevets <begik4@gmail.com>
2024-03-22 23:32:52 +0200

    Updated programs_levels

--- project/routes/programs_levels.py
+++ project/routes/programs_levels.py
@@ -1,31 +1,43 @@
 from flask_restx import Resource, Namespace, abort
 
-from project.extensions import db
-from project.schema import program_level_model
+from project.extensions import db, pagination
+from project.schema import (
+    program_level_model,
+    pagination_parser,
+    custom_schema_pagination,
+)
 from project.models import ProgramLevel
 
-program_level = Namespace(
+program_level_ns = Namespace(
     name="programs_levels", description="Level of the educational program"
 )
 
 
-@program_level.route("/")
+@program_level_ns.route("/")
+@program_level_ns.response(200, model=[program_level_model], description="Success")
 class ProgramLevelList(Resource):
     """Shows a list of all programs levels, and lets you POST to add new program level"""
 
-    @program_level.marshal_list_with(program_level_model)
+    @program_level_ns.expect(pagination_parser)
     def get(self):
         """List all programs levels"""
-        return ProgramLevel.query.all()
+        return pagination.paginate(
+            ProgramLevel,
+            program_level_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
 
-    @program_level.expect(program_level_model)
-    @program_level.marshal_list_with(program_level_model)
+    @program_level_ns.expect(program_level_model, pagination_parser)
     def post(self):
         """Create a new programs levels"""
-        program = ProgramLevel(name=program_level.payload["name"])
+        program = ProgramLevel(name=program_level_ns.payload["name"])
         db.session.add(program)
         db.session.commit()
-        return program, 201
+        return pagination.paginate(
+            ProgramLevel,
+            program_level_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
 
 
 def get_program_level_or_404(id):
@@ -35,29 +47,38 @@ def get_program_level_or_404(id):
     return program
 
 
-@program_level.route("/<int:id>/")
-@program_level.response(404, "Program level not found")
-@program_level.param("id", "The program level unique identifier")
+@program_level_ns.route("/<int:id>/")
+@program_level_ns.response(200, model=[program_level_model], description="Success")
+@program_level_ns.response(404, "Program level not found")
+@program_level_ns.param("id", "The program level unique identifier")
 class ProgramLevelDetail(Resource):
     """Show a single program level and lets you delete them"""
 
-    @program_level.marshal_with(program_level_model)
+    @program_level_ns.marshal_with(program_level_model)
     def get(self, id):
         """Fetch a given program level"""
         return get_program_level_or_404(id)
 
-    @program_level.expect(program_level_model)
-    @program_level.marshal_list_with(program_level_model)
+    @program_level_ns.expect(program_level_model, pagination_parser)
     def put(self, id):
         """Update a program level given its identifier"""
         program = get_program_level_or_404(id)
-        program.name = program_level.payload["name"]
+        program.name = program_level_ns.payload["name"]
         db.session.commit()
-        return program
+        return pagination.paginate(
+            ProgramLevel,
+            program_level_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
 
+    @program_level_ns.expect(pagination_parser)
     def delete(self, id):
         """Delete a program level given its identifier"""
         program = get_program_level_or_404(id)
         db.session.delete(program)
         db.session.commit()
-        return {}, 204
+        return pagination.paginate(
+            ProgramLevel,
+            program_level_model,
+            pagination_schema_hook=custom_schema_pagination,
+        )
