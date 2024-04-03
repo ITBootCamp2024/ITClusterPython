de9d5e8a97c9b6550b7ed812a07cd70dfe618935
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-04-03 17:35:30 +0300

    added short_education_level_model

--- project/schemas/education_levels.py
+++ project/schemas/education_levels.py
@@ -3,23 +3,32 @@ from flask_restx import fields
 from project.extensions import api
 from project.schemas.pagination import get_pagination_schema_for
 
-education_level_model = api.model(
-    "EducationLevel",
+
+short_education_level_model = api.model(
+    "ShortEducationLevel",
     {
         "id": fields.Integer(
             readonly=True, description="Unique identifier of the education level"
-        ),
-        "education_level": fields.String(
-            required=True,
-            description="Education level",
-            max_length=45,
-            default="education level",
         ),
         "name": fields.String(
             required=True,
             description="Education level name",
             max_length=45,
             default="education level name",
+        )
+    }
+)
+
+
+education_level_model = api.model(
+    "EducationLevel",
+    {
+        **short_education_level_model,
+        "education_level": fields.String(
+            required=True,
+            description="Education level",
+            max_length=45,
+            default="education level",
         )
     }
 )
