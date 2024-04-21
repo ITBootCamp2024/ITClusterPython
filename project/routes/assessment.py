11f8209ad86f1c3628d868901fa742e9f130eb06
Maksym Bezhevets <begik4@gmail.com>
2024-04-21 14:32:45 +0300

    Added Assessment get, patch, delete

--- project/routes/assessment.py
+++ project/routes/assessment.py
@@ -12,6 +12,21 @@ assessment_ns = Namespace(
 )
 
 
+def get_assessment_or_404(id):
+    assessment = Assessment.query.get(id)
+    if not assessment:
+        abort(404, "Assessment not found")
+    return assessment
+
+
+def get_assessment_response():
+    assessment = Assessment.query.all()
+    return {
+        "content": assessment,
+        "totalElements": len(assessment)
+    }
+
+
 @assessment_ns.route("/create")
 class AssessmentCreate(Resource):
     """Create a new assessment"""
@@ -38,3 +53,37 @@ class AssessmentCreate(Resource):
 
         return assessment
 
+
+@assessment_ns.route("/<int:id>")
+@assessment_ns.response(404, "Assessment not found")
+@assessment_ns.param("id", "The assessment unique identifier")
+class TeachersDetail(Resource):
+    """Show a assessment and lets you delete him"""
+
+    @assessment_ns.marshal_with(assessment_model)
+    def get(self, id):
+        """Fetch the assessment with a given id"""
+        return get_assessment_or_404(id)
+
+    @assessment_ns.expect(assessment_model, validate=False)
+    @assessment_ns.marshal_with(assessment_model)
+    def patch(self, id):
+        """Update the assessment with a given id"""
+        assessment = get_assessment_or_404(id)
+        plain_params = ["syllabus_id", "object", "method", "tool"]
+        for key, value in assessment_ns.payload.items():
+            if key in plain_params:
+                setattr(assessment, key, value)
+
+        db.session.commit()
+        return Assessment.query.all()
+
+    @allowed_roles(["teacher", "admin", "content_manager"])
+    @assessment_ns.doc(security="jsonWebToken")
+    @assessment_ns.marshal_with(assessment_model)
+    def delete(self, id):
+        """Delete the assessment with given id"""
+        assessment = get_assessment_or_404(id)
+        db.session.delete(assessment)
+        db.session.commit()
+        return Assessment.query.all()
